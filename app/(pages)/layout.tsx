ea9b87f32ba86b9725225495bdda1315a4305ebe
Aqib Akinyele <hackibkenn@gmail.com>
2024-09-21 22:56:51 +0100

    first commit

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export default function App({ children }: LayoutProps) {
   return (
-    <div className="bg-slate-500 py-5">
+    <div className="bg-white md:bg-slate-500 py-5">
       <div className="max-w-sm mx-auto ">{children}</div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import WelcomePage from "./welcome/page";
 
 export default function App() {
   return (
-    <div className="bg-slate-500 py-5">
+    <div className="bg-white md:bg-slate-500 py-5">
       <div className="max-w-sm mx-auto">
         <WelcomePage />
       </div>
