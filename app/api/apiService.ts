70a91816b1d2cf36ab15e3ceba6295530be648b4
Aqib Akinyele <hackibkenn@gmail.com>
2024-09-21 22:48:29 +0100

    first commit

--- app/api/apiService.ts
+++ app/api/apiService.ts
@@ -1,7 +1,7 @@
-import axios from "../utils/axios";
-import { API_AUTH_URL } from "../config";
+// import axios from "../utils/axios";
+// import { API_AUTH_URL } from "../config";
 
-export const fetchCustomerData = async () => {
-  const response = await axios().get(`/users?limit=0`);
-  return response.data;
-};
+// export const fetchCustomerData = async () => {
+//   const response = await axios().get(`/users?limit=0`);
+//   return response.data;
+// };
