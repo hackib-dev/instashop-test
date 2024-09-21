ab6376b84dd18d5c04ae832242a745ab63381300
Aqib Akinyele <hackibkenn@gmail.com>
2024-09-21 22:46:25 +0100

    first commit

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -1,44 +1,44 @@
-import Axios, { AxiosInstance } from "axios";
-import { API_BASE_URL, sessionStorageName } from "../config";
-import { Product } from "@/types.ts";
+// import Axios, { AxiosInstance } from "axios";
+// import { API_BASE_URL, sessionStorageName } from "../config";
+// import { Product } from "@/types.ts";
 
-const axiosInstance: AxiosInstance = Axios.create({
-  baseURL: API_BASE_URL,
-  timeout: 60000, // 1 minutes
-});
+// const axiosInstance: AxiosInstance = Axios.create({
+//   baseURL: API_BASE_URL,
+//   timeout: 60000, // 1 minutes
+// });
 
-const axios = (url?: string, refreshToken?: string) => {
-  if (refreshToken) {
-    axiosInstance.defaults.headers.Authorization = refreshToken;
-  }
-  if (url) {
-    axiosInstance.defaults.baseURL = `${API_BASE_URL}${url}`;
-  }
+// const axios = (url?: string, refreshToken?: string) => {
+//   if (refreshToken) {
+//     axiosInstance.defaults.headers.Authorization = refreshToken;
+//   }
+//   if (url) {
+//     axiosInstance.defaults.baseURL = `${API_BASE_URL}${url}`;
+//   }
 
-  return axiosInstance;
-};
+//   return axiosInstance;
+// };
 
-axiosInstance.interceptors.request.use(
-  (config: any) => {
-    let storedSession;
-    if (typeof window !== "undefined") {
-      storedSession = sessionStorage.getItem(sessionStorageName);
-    }
+// axiosInstance.interceptors.request.use(
+//   (config: any) => {
+//     let storedSession;
+//     if (typeof window !== "undefined") {
+//       storedSession = sessionStorage.getItem(sessionStorageName);
+//     }
 
-    if (storedSession) {
-      const storedSessionObj: Partial<Product> = JSON.parse(storedSession);
-      const refreshToken = storedSessionObj.refreshToken as string;
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = `${refreshToken}`;
-    }
+//     if (storedSession) {
+//       const storedSessionObj: Partial<Product> = JSON.parse(storedSession);
+//       const refreshToken = storedSessionObj.refreshToken as string;
+//       // eslint-disable-next-line no-param-reassign
+//       config.headers.Authorization = `${refreshToken}`;
+//     }
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
-export const AxiosObject = axiosInstance;
+// export const AxiosObject = axiosInstance;
 
-export default axios;
+// export default axios;
--- app/utils/token.ts
+++ app/utils/token.ts
@@ -1,61 +1,61 @@
-import { addMilliseconds } from "date-fns";
-import axios, { AxiosObject } from "./axios";
-import { API_AUTH_URL, sessionStorageName } from "../config";
-import { Product } from "@/types.ts";
-import { setCookie } from "./cookies";
-import { jwtDecode } from "jwt-decode";
+// import { addMilliseconds } from "date-fns";
+// import axios, { AxiosObject } from "./axios";
+// import { API_AUTH_URL, sessionStorageName } from "../config";
+// import { Product } from "@/types.ts";
+// import { setCookie } from "./cookies";
+// import { jwtDecode } from "jwt-decode";
 
-let storedSession: string | null;
-if (typeof window !== "undefined") {
-  storedSession = sessionStorage.getItem(sessionStorageName);
-}
+// let storedSession: string | null;
+// if (typeof window !== "undefined") {
+//   storedSession = sessionStorage.getItem(sessionStorageName);
+// }
 
-export const refreshAccessToken = async (refreshToken: string) => {
-  const response = await axios(API_AUTH_URL).post(`/refresh`, {
-    refreshToken,
-  });
-  return response.data;
-};
+// export const refreshAccessToken = async (refreshToken: string) => {
+//   const response = await axios(API_AUTH_URL).post(`/refresh`, {
+//     refreshToken,
+//   });
+//   return response.data;
+// };
 
-let refreshTimer: NodeJS.Timeout | null = null;
+// let refreshTimer: NodeJS.Timeout | null = null;
 
-export const stopTokenRefreshTimer = () => {
-  if (refreshTimer) {
-    clearTimeout(refreshTimer);
-    refreshTimer = null;
-  }
-};
+// export const stopTokenRefreshTimer = () => {
+//   if (refreshTimer) {
+//     clearTimeout(refreshTimer);
+//     refreshTimer = null;
+//   }
+// };
 
-export const startTokenRefreshTimer = (
-  expirationTime: number,
-  refreshToken: string
-) => {
-  const expirationMinus120000 = Number(expirationTime) - 120000;
-  const jwtExpirationTime =
-    expirationMinus120000 <= 0 ? 1000 : expirationMinus120000;
+// export const startTokenRefreshTimer = (
+//   expirationTime: number,
+//   refreshToken: string
+// ) => {
+//   const expirationMinus120000 = Number(expirationTime) - 120000;
+//   const jwtExpirationTime =
+//     expirationMinus120000 <= 0 ? 1000 : expirationMinus120000;
 
-  refreshTimer = setTimeout(async () => {
-    try {
-      const { token: newToken, refreshToken: newExpirationTime } =
-        await refreshAccessToken(refreshToken);
-      const decodedToken = jwtDecode<{ exp: number }>(newExpirationTime);
-      const ttl = decodedToken?.exp;
-      const tokenExpiration = addMilliseconds(new Date(), ttl);
-      setCookie("ttl", tokenExpiration.toISOString(), tokenExpiration);
+//   refreshTimer = setTimeout(async () => {
+//     try {
+//       const { token: newToken, refreshToken: newExpirationTime } =
+//         await refreshAccessToken(refreshToken);
+//       const decodedToken = jwtDecode<{ exp: number }>(newExpirationTime);
+//       const ttl = decodedToken?.exp;
+//       const tokenExpiration = addMilliseconds(new Date(), ttl);
+//       setCookie("ttl", tokenExpiration.toISOString(), tokenExpiration);
 
-      if (storedSession) {
-        const storedSessionObj: Partial<Product> = JSON.parse(storedSession);
-        storedSessionObj.token = newToken;
-        sessionStorage.setItem(
-          sessionStorageName,
-          JSON.stringify(storedSessionObj)
-        );
-      }
-      startTokenRefreshTimer(Number(newExpirationTime), newToken);
-    } catch (error) {
-      console.error(error);
-      sessionStorage.clear();
-      stopTokenRefreshTimer();
-    }
-  }, jwtExpirationTime);
-};
+//       if (storedSession) {
+//         const storedSessionObj: Partial<Product> = JSON.parse(storedSession);
+//         storedSessionObj.token = newToken;
+//         sessionStorage.setItem(
+//           sessionStorageName,
+//           JSON.stringify(storedSessionObj)
+//         );
+//       }
+//       startTokenRefreshTimer(Number(newExpirationTime), newToken);
+//     } catch (error) {
+//       console.error(error);
+//       sessionStorage.clear();
+//       stopTokenRefreshTimer();
+//     }
+//   }, jwtExpirationTime);
+// };
