ad0f0b98acaf3ed6da77f0ed6bc917aa8f6d3554
Aqib Akinyele <hackibkenn@gmail.com>
2024-09-22 10:14:37 +0100

    fixes

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export default function App({ children }: LayoutProps) {
   return (
-    <div className="bg-white md:bg-slate-500 py-5">
+    <div className="bg-white md:bg-slate-200 py-5">
       <div className="w-full xl:max-w-sm mx-auto ">{children}</div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import WelcomePage from "./welcome/page";
 
 export default function App() {
   return (
-    <div className="">
+    <div className="bg-white md:bg-slate-200">
       <div className="w-screen xl:max-w-sm mx-auto pt-20 ">
         <WelcomePage />
       </div>
